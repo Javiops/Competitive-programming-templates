a57ac8e7a1b76926e32dfa452c9389a93849cd61
Javiops <152914603+Javiops@users.noreply.github.com>
2026-03-10 17:51:36 +0100

    Add Lazy Update Treap implementation
    Implement a lazy update treap for efficient range reversals.

---
+++ Data-Structures/Treaps/Lazy_update_treap.cpp
@@ -0,0 +1,111 @@
+//https://cses.fi/problemset/task/2073/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937 rng(1337);
+
+struct tnode {
+    int pri, size;
+    char s;
+    bool rev;
+    tnode *c[2];
+    tnode (char t) {        
+        pri = rng(), size = 1;
+        rev = 0;
+        s = t;
+        c[0] = c[1] = NULL;
+    }
+};
+
+int size(tnode *treap) { return treap ? treap->size : 0; }
+
+void push(tnode *t) {
+    if (t && t->rev) {
+        t->rev = 0;           // Clear the flag
+        swap(t->c[0], t->c[1]);   // Swap the children
+        
+        // Pass the lazy flag down to the children
+        if (t->c[0]) t->c[0]->rev ^= 1; 
+        if (t->c[1]) t->c[1]->rev ^= 1;
+    }
+}
+
+void update(tnode *t) {
+    if (t) {
+        t->size = 1 + size(t->c[0]) + size(t->c[1]);
+    }
+}
+
+pair<tnode*,tnode*> split(tnode* t, int k) { 
+   // return left and right treaps containing lower and higher values, respectively
+   // nodes with value >= v will go to the right
+   if (!t) return {t,t};
+   push(t);
+   if (size(t->c[0]) >= k) { // our current node will be in the left treap
+       auto p = split(t->c[0], k);
+       t->c[0] = p.second; 
+       
+       update(t);
+       
+       return {p.first, t};
+   } else { // our current node will be in the right treap
+       auto p = split(t->c[1], k - size(t->c[0]) - 1);
+       t->c[1] = p.first;
+       
+       update(t);
+       
+       return {t, p.second};
+   }
+}
+
+tnode* merge(tnode* l, tnode* r) {
+   if (!l) return r;
+   if (!r) return l;
+   push(l);
+   push(r);
+   if (l->pri > r->pri) { // maintain heap property, l will be topmost node of treap
+       l->c[1] = merge(l->c[1],r);
+       update(l);
+       return l;
+   } else {
+       r->c[0] = merge(l,r->c[0]);
+       update(r);
+       return r;
+   }
+}
+
+void print(tnode *t){
+    if (!t) return;
+    push(t);
+    print(t->c[0]);
+    cout << t->s;
+    print(t->c[1]);
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, q; cin >> n >> q;
+    string s; cin >> s;
+    tnode *t = NULL;
+    
+    for (int i = 0; i < n; i++){
+        t = merge(t, new tnode(s[i]));
+    }
+    while(q--){
+        int l, r; cin >> l >> r;
+        l--; r--;
+        
+        auto a = split(t, l);
+        auto b = split(a.second, r - l + 1);
+        
+        if (b.first) b.first->rev ^= 1;
+        
+        t = merge(merge(a.first, b.first), b.second);
+    }
+    print(t);
+    cout << endl;
+}
